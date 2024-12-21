fc8b0d9d77ee8ae79ebdf34203d78ec7b74e63bf
sarangyang <sarang628@gmail.com>
2024-12-22 01:40:29 +0900

    Update .gitignore file

--- library/src/main/java/com/sryang/library/pullrefresh/RefreshIndicatorState.kt
+++
@@ -1,8 +0,0 @@
-package com.sryang.library.pullrefresh
-
-enum class RefreshIndicatorState(msg: String) {
-    Default("Default"),
-    PullingDown("PullingDown"),
-    ReachedThreshold("ReachedThreshold"),
-    Refreshing("Refreshing")
-}
